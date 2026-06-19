fa693efddabeff682eff203e81cec22df777f8cd
DooPrjkt <gitorrb@gmail.com>
2026-06-19 00:56:24 +0000

    Add config

--- BoardCommon.mk
+++ BoardCommon.mk
@@ -11,6 +11,9 @@ LOCAL_PATH := vendor/prize/camera
 # Properties
 TARGET_SYSTEM_EXT_PROP += $(LOCAL_PATH)/system_ext.prop
 
+# Build Hacks
+BUILD_BROKEN_DUP_SYSPROP := true
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy/vendor
