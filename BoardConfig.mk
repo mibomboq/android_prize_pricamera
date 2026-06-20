11a02cc0b2b3f870ac7a3c0c5815394738ffe411
DooPrjkt <gitorrb@gmail.com>
2026-06-21 01:18:07 +0700

    X1: initial Pricamera -sepolicy by luigiee
    Signed-off-by: DooPrjkt <gitorrb@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+include vendor/advan/X1/X1-vendor.mk
+
+PRIZE_PATH := device/prize/camera
+
+# Build Hacks
+BUILD_BROKEN_DUP_SYSPROP := true
+
+# Properties
+TARGET_SYSTEM_EXT_PROP += $(PRIZE_PATH)/system_ext.prop
+
+# Sepolicy
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(PRIZE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(PRIZE_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(PRIZE_PATH)/sepolicy/vendor
---
+++ common.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/xiaomi/miuicamera/common/common-vendor.mk)
+
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    PriCamera
+
+# Priv-app permission
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-pricamera.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-pricamera.xml
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pricamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/pricamera-hiddenapi-package-allowlist.xml
